d036938b9a8e4ea4c1720224e05ac46e500316a8
CherepashkaD <154900200+CherepashkaD@users.noreply.github.com>
2024-03-29 16:05:29 +0300

    Add files via upload

---
+++ java-script.js
@@ -0,0 +1,10 @@
+// $(document).ready (function() {
+//     $( function() {
+//         $( ".box" ).draggable();
+//     } );
+// });
+
+// $(".fb").click(funrtion(){
+//     $(".section1").addClass("display_none").removeClass("display_block");
+//     $(".section2").addClass("display_block").removeClass("display_none");
+// });
